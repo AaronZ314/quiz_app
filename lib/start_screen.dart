d95a118980c2422633f2fe25d00e96bb6312c61e
AaronZ314 <aaronminxz@gmail.com>
2026-02-10 11:26:15 -0500

    Finished initial start screen layout

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget{
+class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 63, 8, 185),
-          Color.fromARGB(255, 12, 1, 39)
-        ],
-        begin:Alignment.topLeft,
-        end:Alignment.bottomRight,
-        )
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 63, 8, 185),
+            Color.fromARGB(255, 12, 1, 39),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
       ),
-      child:Center(child: Text("Start Quiz"),)
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png', width: 280),
+            const SizedBox(height: 80),
+            Text(
+              "Challenge yourself on this quiz!",
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            const SizedBox(height: 30),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              child: Text("Start Quiz"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
