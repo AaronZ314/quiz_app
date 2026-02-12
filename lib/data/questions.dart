0eb81d489430143977a9e0f2df51a6d3e3211e9b
AaronZ314 <aaronminxz@gmail.com>
2026-02-12 11:39:45 -0500

    added quiz questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,66 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+//For consistency the first answer is always the correct one
+
+const questions = [
+  QuizQuestion(
+    'Are bananas a berry'
+    ,
+    [
+      'Yes',
+      'No',
+      'What',
+      'You suck',
+    ]
+  ),
+  QuizQuestion(
+    'Are tomatos a fruit'
+    ,
+    [
+      'Yes',
+      'No',
+      'Kind of',
+      'Its spelled tomatoe',
+    ]
+  ),
+  QuizQuestion(
+    'What is the fruit with the most fat'
+    ,
+    [
+      'Avocado',
+      'Mango',
+      'Tangerine',
+      'Apple',
+    ]
+  ),
+  QuizQuestion(
+    'Are pineapples technically berries'
+    ,
+    [
+      'Yes',
+      'No',
+      'What is a pineapple',
+      'I dont know stop asking me questions',
+    ]
+  ),
+  QuizQuestion(
+    'What fruit has the highest water content'
+    ,
+    [
+      'Watermelon',
+      'Durian',
+      'Banana',
+      'Lychee',
+    ]
+  ),
+  QuizQuestion(
+    'What is the oldest cultivated fruit'
+    ,
+    [
+      'Figs',
+      'Cherries',
+      'Oranges',
+      'Pears',
+    ]
+  ),
+];
