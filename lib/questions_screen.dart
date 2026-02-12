3a575fcc3cdf96735e9f47a69734ee7c271cfb82
AaronZ314 <aaronminxz@gmail.com>
2026-02-12 12:11:27 -0500

    Made answer question widget and put it on the screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -21,10 +22,10 @@ class _QuestionScreenState extends State<QuestionsScreen> {
             style: TextStyle(color: Colors.white, fontSize: 28),
           ),
           const SizedBox(height: 30),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 4')),
+          AnswerButton(answerText: "Answer 1", onTap: (){}),
+          AnswerButton(answerText: "Answer 2", onTap: (){}),
+          AnswerButton(answerText: "Answer 3", onTap: (){}),
+          AnswerButton(answerText: "Answer 4", onTap: (){}),
         ],
       ),
     );
