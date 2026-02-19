06422d09f233ae0f551703a0a0fcd7cb46a6bb01
AaronZ314 <aaronminxz@gmail.com>
2026-02-19 12:03:43 -0500

    did basic styling on answer buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -16,19 +16,24 @@ class _QuestionScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.question,
-            style: TextStyle(color: Colors.white, fontSize: 28),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((item){
-            return AnswerButton(answerText: item,
-            onTap: () {});
-          }),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: TextStyle(color: Colors.white, fontSize: 28),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((item){
+              return AnswerButton(answerText: item,
+              onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
